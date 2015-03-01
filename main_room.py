ff4e0c5a8494448b8f9410996b145f9af48126c9
Luis Lopez <luislpz978@rocketmail.com>
2015-03-01 15:32:08 -0600

    Implemented the animation state machine for the player

--- main_room.py
+++ main_room.py
@@ -3,13 +3,23 @@ from world import *
 from engine import *
 
 from scripts import *
-import os
-import re
+from utility import *
+from state_machine import *
 
 engine = Engine(1200, 700)
 
 # load music to play in the background
 background_music = mixer.music.load("assets/music/MarysCreepyCarnivalTheme.mp3")
+
+
+class UpdateAnimationHandler(WorldScript):
+
+    def __init__(self, anim_state_machine):
+        super(UpdateAnimationHandler, self).__init__("animation handler")
+        self.anim_state_machine = anim_state_machine
+
+    def update(self):
+        self.anim_state_machine.update()
 
 
 class PlatformWorld(World):
@@ -18,7 +28,9 @@ class PlatformWorld(World):
         super(PlatformWorld, self).__init__()
 
         self.player = None
-        self.player_anims = dict()
+        self.player_anim_handler = None
+
+        self.ladders = list()
 
     def load_scene(self):
         w = self.engine.display.get_width()
@@ -59,15 +71,17 @@ class PlatformWorld(World):
         mixer.music.play(-1)
         mixer.music.set_volume(0.5)
 
+        self.add_script(UpdateAnimationHandler(self.player_anim_handler))
+
     def load_ladders(self):
         path = "assets/images/ladders/"
         load = pygame.image.load
         ladder_body = load(path + "ladder_body.png").convert_alpha()
         ladder_top = load(path + "ladder_top.png").convert_alpha()
 
-        create_ladder(self, ladder_body, ladder_top, 440, 950, 395)
-        create_ladder(self, ladder_body, ladder_top, 270, 1650, 25)
-        create_ladder(self, ladder_body, ladder_top, 410, 1920, 395)
+        self.create_ladder(ladder_body, ladder_top, 440, 950, 395)
+        self.create_ladder(ladder_body, ladder_top, 270, 1650, 25)
+        self.create_ladder(ladder_body, ladder_top, 410, 1920, 395)
 
     def load_platforms(self):
 
@@ -160,7 +174,7 @@ class PlatformWorld(World):
         w = self.engine.display.get_width()
         h = self.engine.display.get_height()
 
-        floor_tile = pygame.image.load("assets/images/floor_tile.png").convert()
+        floor_tile = pygame.image.load("assets/images/floors/floor_tile.png").convert()
 
         img = create_img_from_tile(floor_tile, w*2, 200)
         floor_a = self.create_game_object(img)
@@ -204,7 +218,9 @@ class PlatformWorld(World):
         # load animation frames
         self.load_anims()
 
-        self.player = self.create_game_object(self.player_anims["Idle"].frames[0])
+        starting_image = self.player_anim_handler.current_state.animation.frames[0]
+
+        self.player = self.create_game_object(starting_image)
         self.player.add_component(RigidBody())
         self.player.transform.position = Vector2(230, 480)
         self.player.transform.scale = Vector2(1, 1)
@@ -214,17 +230,13 @@ class PlatformWorld(World):
         self.player.name = "player"
 
         self.player.collider.set_box(50, 80)
-        self.player.collider.offset = Vector2(-12, 5)
+        self.player.collider.set_offset(-12, 5)
 
         self.player.add_script(PlayerClimbing("player climb"))
         self.player.add_script(PlayerPlatformMovement("player plat move"))
 
-        # add animator to player
-        animator = Animator()
-        self.player.add_component(animator)
-
-        # set the first animation
-        animator.set_animation(self.player_anims["Idle"])
+        # add animator to player from the animation state machine
+        self.player.add_component(self.player_anim_handler.animator)
 
     def load_elevators(self):
 
@@ -272,160 +284,87 @@ class PlatformWorld(World):
 
     def load_anims(self):
 
+        self.player_anim_handler = AnimationStateMachine(Animator())
+
         path_to_anims = "assets/animations/"
 
         # setup the idle animation
-        animation = load_anim_from_directory(path_to_anims + "Idle/")
+        anim = load_anim_from_directory(path_to_anims + "Idle/")
 
         # set time between frames in seconds
-        animation.frame_latency = 0.18
+        anim.frame_latency = 0.18
 
-        # add the animation to the dictionary
-        self.player_anims["Idle"] = animation
+        # create a state and load it to the state machine
+        state = AnimationStateMachine.AnimationState("idle", anim)
+        self.player_anim_handler.add_state(state)
 
         # setup the walk animation
-        animation = load_anim_from_directory(path_to_anims + "Walking/")
-        animation.frame_latency = 0.1
-        self.player_anims["Walking"] = animation
+        anim = load_anim_from_directory(path_to_anims + "Walking/")
+        anim.frame_latency = 0.1
+        state = AnimationStateMachine.AnimationState("walking", anim)
+        self.player_anim_handler.add_state(state)
 
         # add jump animation
-        animation = load_anim_from_directory(path_to_anims + "Jumping/")
-        animation.frame_latency = 0.12
-        animation.cycle = False
-        self.player_anims["Jumping"] = animation
+        anim = load_anim_from_directory(path_to_anims + "Jumping/")
+        anim.frame_latency = 0.12
+        anim.cycle = False
+        state = AnimationStateMachine.AnimationState("jumping", anim)
+        self.player_anim_handler.add_state(state)
 
         # climb animation
-        animation = load_anim_from_directory(path_to_anims + "Climbing/")
-        animation.frame_latency = 0.1
-        self.player_anims["Climbing"] = animation
+        anim = load_anim_from_directory(path_to_anims + "Climbing/")
+        anim.frame_latency = 0.12
+        state = AnimationStateMachine.AnimationState("climbing", anim)
+        self.player_anim_handler.add_state(state)
 
+        self.player_anim_handler.set_current_state("idle")
 
-def set_floor_attributes(floor):
-    floor.collider.restitution = 0
-    floor.collider.surface_friction = 0.75
-    floor.renderer.depth = -10
-    floor.tag = "floor"
+        # transitions for the state machine
+        walk_transition = StateMachine.Transition()
+        idle_transition = StateMachine.Transition()
+        jump_transition = StateMachine.Transition()
+        climb_transition = StateMachine.Transition()
 
+        # add conditions to the transitions
+        # test to see if the player is moving on the x-axis
+        walk_transition.add_condition(lambda: abs(self.player.rigid_body.velocity.x) > 50)
+        walk_transition.add_condition(lambda: self.player.get_script("player plat move").grounded)
+        walk_transition.add_condition(lambda: not self.player.get_script("player climb").climbing)
 
-def set_wall_attributes(wall):
-    wall.collider.restitution = 0
-    wall.collider.surface_friction = 0.8
-    wall.tag = "wall"
+        idle_transition.add_condition(lambda: abs(self.player.rigid_body.velocity.x) < 50)
+        idle_transition.add_condition(lambda: self.player.get_script("player plat move").grounded)
+        idle_transition.add_condition(lambda: not self.player.get_script("player climb").climbing)
 
+        jump_transition.add_condition(lambda: not self.player.get_script("player plat move").grounded and False)
 
-def set_ceiling_attributes(ceiling):
-    ceiling.collider.restitution = 0
-    ceiling.collider.surface_friction = 0.8
-    ceiling.tag = "ceiling"
+        climb_transition.add_condition(lambda: self.player.get_script("player climb").colliding_with_ladder())
+        climb_transition.add_condition(lambda: self.player.get_script("player climb").climbing)
 
+        # set up transitions between states
+        self.player_anim_handler.add_bi_transition("idle", "walking", walk_transition, idle_transition)
+        self.player_anim_handler.add_bi_transition("idle", "jumping", jump_transition, idle_transition)
+        self.player_anim_handler.add_bi_transition("walking", "jumping", jump_transition, walk_transition)
 
-def set_platform_attributes(platform):
-    platform.renderer.depth = 1
-    platform.collider.restitution = 0
-    platform.collider.surface_friction = 0.75
-    platform.tag = "platform"
+        self.player_anim_handler.add_bi_transition("idle", "climbing", climb_transition, idle_transition)
+        self.player_anim_handler.add_bi_transition("walking", "climbing", climb_transition, walk_transition)
+        self.player_anim_handler.add_bi_transition("jumping", "climbing", climb_transition, jump_transition)
 
+    def create_ladder(self, ladder_body, ladder_top, height, x, y):
+        img = create_img_from_tile(ladder_body, ladder_body.get_width(), height)
 
-def create_ladder(world, ladder_body, ladder_top, height, x, y):
-    img = create_img_from_tile(ladder_body, ladder_body.get_width(), height)
+        # pad image on top of the ladder
+        img = conjoin_surfaces_vertically(ladder_top, img)
 
-    # pad image on top of the ladder
-    img = conjoin_surfaces_vertically(ladder_top, img)
+        ladder = self.create_game_object(img)
+        ladder.collider.is_trigger = True
+        ladder.transform.position = Vector2(x, y)
 
-    ladder1 = world.create_game_object(img)
-    ladder1.collider.is_trigger = True
-    ladder1.transform.position = Vector2(x, y)
+        ladder.collider.box.w -= 80
+        ladder.collider.box.h -= 50
 
-    ladder1.collider.box.w -= 80
-    ladder1.collider.box.h -= 50
+        ladder.tag = "ladder"
 
-    ladder1.tag = "ladder"
-
-
-def get_files_in_dir(dir_path):
-
-    directory = os.listdir(dir_path)
-
-    # a list to store the paths to the individual files
-    file_paths = list()
-    for file_ in directory:
-
-        # add the whole relative path
-        file_paths.append(dir_path + file_)
-
-    # do a natural sort on the file names, meaning that string numbers are sorted
-    # by numeric values.
-    file_paths = natural_sort(file_paths)
-    return file_paths
-
-
-# This goes to a directory where each file represents an individual
-# animation frame. This returns an animation object with the loaded frames.
-def load_anim_from_directory(dir_path):
-
-    file_list = get_files_in_dir(dir_path)
-
-    # set up animation
-    animation = Animator.Animation()
-    for file_ in file_list:
-        frame = pygame.image.load(file_).convert_alpha()
-        animation.add_frame(frame)
-
-    return animation
-
-
-# Excerpt from stack overflow, Mark Byers
-def natural_sort(l):
-    convert = lambda text: int(text) if text.isdigit() else text.lower()
-    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
-    return sorted(l, key=alphanum_key)
-
-
-# given a tile image, this function will create a quilt of tiles onto a another surface
-# given the width and height.
-def create_img_from_tile(tile_surface, width, height):
-
-    tile_w = tile_surface.get_width()
-    tile_h = tile_surface.get_height()
-
-    size = (width, height)
-    dst_surface = pygame.Surface(size).convert()
-
-    # make the dst_surface transparent
-    color_mask = (123, 54, 33)
-    dst_surface.fill(color_mask)
-    dst_surface.set_colorkey(color_mask)
-
-    # fill the dst_surface horizontally first with the tile, once we get to the edge
-    # go down a column and repeat.
-    for y in range(0, height, tile_h):
-        for x in range(0, width, tile_w):
-            dst_surface.blit(tile_surface, (x, y))
-
-    return dst_surface
-
-
-# conjoins surface b on the bottom of surface a
-def conjoin_surfaces_vertically(surface_a, surface_b):
-    width = max(surface_a.get_width(), surface_b.get_width())
-    height = surface_a.get_height() + surface_b.get_height()
-
-    size = (width, height)
-    dst_surface = pygame.Surface(size).convert()
-
-    # make the dst_surface transparent
-    color_mask = (123, 54, 33)
-    dst_surface.fill(color_mask)
-    dst_surface.set_colorkey(color_mask)
-
-    # center each surface relative to the destination surface
-    center_a = width/2 - surface_a.get_width()/2
-    center_b = width/2 - surface_b.get_width()/2
-
-    dst_surface.blit(surface_a, (center_a, 0))
-    dst_surface.blit(surface_b, (center_b, surface_a.get_height()))
-
-    return dst_surface
+        self.ladders.append(ladder)
 
 
 engine.set_world(PlatformWorld())
